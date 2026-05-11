1c2764d00b390e041af6c2b59328271b023217c0
RyanRLai <157196161+RyanRLai@users.noreply.github.com>
2026-05-11 19:47:23 -0400

    Add data model package

---
+++ src/models/__init__.py
@@ -0,0 +1,8 @@
+from .records import EvidenceRecord, IncomeRecord, ParsedDocumentLog, TaxResult
+
+__all__ = [
+    "EvidenceRecord",
+    "IncomeRecord",
+    "ParsedDocumentLog",
+    "TaxResult",
+]
